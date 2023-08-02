59c44e89f72566f80fcba7cc71a7a46d43093783
Wilner Claro <wilnerclaro@gmail.com>
2023-08-02 12:41:24 -0300

    projeto criado

---
+++ src/main/java/br/com/wilner/controleFinanceiro/ControleFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.wilner.controleFinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFinanceiroApplication.class, args);
+	}
+
+}
